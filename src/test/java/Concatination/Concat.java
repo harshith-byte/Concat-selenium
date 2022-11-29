3419295c0b7c0046bf5d089435cfd9d415c313db
Harshith.R <63989093+harshith-byte@users.noreply.github.com>
2022-11-29 21:18:18 +0530

    Add files via upload

---
+++ src/test/java/Concatination/Concat.java
@@ -0,0 +1,15 @@
+package Concatination;
+
+import org.testng.annotations.Test;
+
+public class Concat {
+
+	@Test
+	public void concatString() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello "+ "Harshith "+"R");
+		
+	}
+
+}
